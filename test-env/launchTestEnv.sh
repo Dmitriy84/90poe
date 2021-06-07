30887f5547b3fcf43a6b181c47e184e20a4dc7ed
dmytro.malohlovets@intapp.com <dmytro.malohlovets@gmail.com>
2021-06-07 15:11:31 +0300

    initial commit

---
+++ test-env/launchTestEnv.sh
@@ -0,0 +1,9 @@
+cd ..
+mvn -DskipTests clean package spring-boot:repackage
+docker build --tag=dmytromalohlovets/message-server:latest .
+# docker.io
+# docker push dmytromalohlovets/message-server:latest
+cd test-env
+
+docker-compose up -d --force-recreate --remove-orphans
+docker-compose exec kafka0  kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic devTesTask90POE
