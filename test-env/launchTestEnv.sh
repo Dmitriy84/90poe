f49abafba9a5a9e6c366327c32b867a0a8ac1b66
dmytro.malohlovets@intapp.com <dmytro.malohlovets@gmail.com>
2021-06-07 15:54:58 +0300

    test plan left

--- test-env/launchTestEnv.sh
+++ test-env/launchTestEnv.sh
@@ -1,9 +1,13 @@
-cd ..
-mvn -DskipTests clean package spring-boot:repackage
-docker build --tag=dmytromalohlovets/message-server:latest .
+#!/bin/bash
+if [[ $1 == 'build' ]]
+then
+  cd ..
+  mvn -DskipTests clean package spring-boot:repackage
+  docker build --tag=dmytromalohlovets/message-server:latest .
 # docker.io
-# docker push dmytromalohlovets/message-server:latest
-cd test-env
+  docker push dmytromalohlovets/message-server:latest
+  cd test-env
+fi
 
 docker-compose up -d --force-recreate --remove-orphans
 docker-compose exec kafka0  kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic devTesTask90POE
